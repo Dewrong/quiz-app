10a0a976077843cfbbff189513533a3abeabc52c
Dewrong <ysidda33@gmail.com>
2026-02-15 22:36:47 -0600

    Added reveal answers button

--- src/components/finish-page.jsx
+++ src/components/finish-page.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
 function FinishPage({ questionBank, userAnswers, resetQuiz }) {
-  console.log(questionBank, userAnswers);
   const finalScore = getScore();
+  const [showAnswers, setShowAnswers] = useState(false);
 
   function getScore() {
     let score = 0;
@@ -20,6 +20,33 @@ function FinishPage({ questionBank, userAnswers, resetQuiz }) {
       <h2>
         You scored {finalScore}/{questionBank.length}
       </h2>
+      <button
+        className="restart-button"
+        onClick={() => setShowAnswers(!showAnswers)}
+      >
+        Reveal Answers
+      </button>
+      {showAnswers && (
+        <div id="answers">
+          {userAnswers.map((answer, index) =>
+            answer !== questionBank[index].answer ? (
+              <div className="answer-incorrect">
+                For Question {index + 1}, "{questionBank[index].question}"{" "}
+                <br />
+                "You chose: "{answer}" Correct answer: "
+                {questionBank[index].answer}"
+              </div>
+            ) : (
+              <div className="answer-correct">
+                For Question {index + 1}, "{questionBank[index].question}"{" "}
+                <br />
+                "You chose: "{answer}" Correct answer: "
+                {questionBank[index].answer}"
+              </div>
+            ),
+          )}
+        </div>
+      )}
       <button className="restart-button" onClick={resetQuiz}>
         Restart Quiz
       </button>
